52ae49d4f6c5a117884da488934551234cd781f5
MickiJensen <s202758@student.dtu.dk>
2021-01-09 17:24:58 +0000

    started a simple one-way anova of Response~DetStock

--- Case1_RScript.R
+++ Case1_RScript.R
@@ -1,6 +1,7 @@
 # HI FRIENDS! 
 
-#################### INITIAL INSPECTION OF DATA ##########################
+#################### START: INITIAL INSPECTION OF DATA ########################
+
 #Load data as SPRdata:
 SPRdata <- read.csv("SPR.txt", header = TRUE, sep = "\t", dec = ".")
 #Get rid of cycle and date - IS THIS CORRECT, I DONT KNOW????
@@ -21,3 +22,19 @@ boxplot(Response ~ EnzymeConc, SPRdata, ylab = "Response")
 ###### Higher response values to be expected in the Det+ experiments and high enzyme concentrations
 ###### as they have higher means. An ANOVA test should be able to test wether the means are significantly
 ###### different from each other.
+
+#################### END: INITIAL INSPECTION OF DATA ##########################
+
+
+#################### START: FIRST ANALYSIS OF POINT 1 IN CASE #################
+# How does hardness detergent influence the catalytic activity
+# Perform one-way Anova:
+lm1 <- lm(Response ~ DetStock, SPRdata)
+anova(lm1)
+# Check model assumptions
+par(mfrow = c(1,2))
+plot(lm1, col = SPRdata$DetStock, which = 1:2)
+# Interpret lm1
+summary(lm1)
+
+#################### END: FIRST ANALYSIS OF POINT 1 IN CASE #################
