2290221cdbdb398f15379bfda4fe1e65d6ff890f
MickiJensen <s202758@student.dtu.dk>
2021-01-09 13:43:58 +0000

    Made some data-management, incl. deleting date and cycle while also inspecting the data

--- Case1_RScript.R
+++ Case1_RScript.R
@@ -1,1 +1,15 @@
 # HI FRIENDS! 
+
+#Load data as SPRdata:
+SPRdata <- read.csv("SPR.txt", header = TRUE, sep = "\t", dec = ".")
+#Get rid of cycle and date - IS THIS CORRECT, I DONT KNOW????
+SPRdata <- subset(SPRdata, select = c("Response", "Enzyme", "EnzymeConc", "DetStock", "CaStock"))
+#Factor the relevant variables:
+SPRdata$Enzyme <- as.factor(SPRdata$Enzyme)
+SPRdata$EnzymeConc <- as.factor(SPRdata$EnzymeConc)
+SPRdata$DetStock <- as.factor(SPRdata$DetStock)
+SPRdata$CaStock <- as.factor(SPRdata$CaStock)
+#Inspect data:
+str(SPRdata)
+summary(SPRdata)
+pairs(SPRdata, panel = panel.smooth, col = SPRdata$Enzyme)
